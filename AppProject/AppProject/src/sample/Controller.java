4c1d3d83567278c6324954362c5d3ca0e92b55ec
Manisha Sharma <Manisha Sharma@LAPTOP-RHI74MVP>
2019-02-27 21:57:15 -0500

    try2

--- AppProject/AppProject/src/sample/Controller.java
+++ AppProject/AppProject/src/sample/Controller.java
@@ -11,8 +11,6 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-import pct.GameDetails;
-import pct.GameparametersDialogController;
 
 import java.io.IOException;
 import java.sql.SQLOutput;
@@ -35,81 +33,5 @@ public class Controller {
         GraphicsContext graphicsContext = canvasId.getGraphicsContext2D();
         Image image = new Image("resources/risk.png");
         graphicsContext.drawImage(image,0,0,canvasId.getWidth(),canvasId.getHeight());
-    }
-    public void startGame() {
-
-        // Intializing the dialog pane and loading the dialog.
-        Dialog<ButtonType> gameParametersdialog = new Dialog<>();
-        gameParametersdialog.initOwner(MainBorderPane.getScene().getWindow());
-        FXMLLoader gameparametersDialogloader = new FXMLLoader();
-        gameparametersDialogloader.setLocation(getClass().getResource("Gameparameters.fxml"));
-
-        try {
-            gameParametersdialog.getDialogPane().setContent(gameparametersDialogloader.load());
-
-        } catch(IOException e){
-            System.out.println("Cannot load the game parameters dialog");
-            e.printStackTrace();
-            return;
-        }
-
-        gameParametersdialog.getDialogPane().getButtonTypes().add(ButtonType.FINISH);
-        gameParametersdialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
-
-        // if the user has entered finish then read the game parameters and store in the GameDetails object.
-        Optional<ButtonType> result = gameParametersdialog.showAndWait();
-        if(result.isPresent() && result.get() == ButtonType.FINISH) {
-            GameparametersDialogController gpdc_controller = gameparametersDialogloader.getController();
-            gpdc_controller.createGameinstance();
-
-            // Get the index of the Game details object of type new game.
-            int index = returnIndex("NEWGAME");
-
-            GameDetails object2 = GameDetails.getGamedetails().getgamedetails().get(index);
-
-            System.out.println("The user selected = "+object2.getNumberOfPlayers());
-
-            // Intialize the player objects and player colors and map.
-            GameDetails.getGamedetails().IntializeColors(index);
-            GameDetails.getGamedetails().IntializePlayers(index);
-            GameDetails.getGamedetails().InitializeArmies(index);
-            GameDetails.getGamedetails().createMap(index);
-
-            // If map is valid then Load the game window.
-            if(GameDetails.getGamedetails().validateMap(index)){
-                System.out.println("Map is valid.");
-                System.out.println("*********************************************************************************");
-                System.out.println("Proceeding to the next step");
-                //System.out.println("The player name was "+GameDetails.getGamedetails().getgamedetails().get(0).getPlayersList().get(0).getPlayerName());
-
-                // To distribute the territories to the players.
-                GameDetails.getGamedetails().distributeTerritories(index);
-
-                // To distribute the no of armies to the players.
-                GameDetails.getGamedetails().distributeArmies(index);
-
-                // To distribute the armies to the territories.
-                GameDetails.getGamedetails().distributeArmiestoTerritories(index);
-
-                System.out.println(GameDetails.getGamedetails().getPlayersList().get(0).getTerritoriesHeld().size());
-                System.out.println(GameDetails.getGamedetails().getPlayersList().get(1).getTerritoriesHeld().size());
-
-                // To get the name of the map with out .map extension.
-                String string = object2.getMapFile().getName();
-                string = string.substring(0,string.length()-4);
-                System.out.println("Map name with out extension is "+ string);
-                GameDetails.getGamedetails().getgamedetails().get(index).setMapName(string);
-
-                loadGamewindow();   // Loads the game window when map is valid.
-            } else {
-                System.out.println("********************************MAP IS INVALID***********************************");
-                System.out.println("----------------------------Cannot Load the Game Window--------------------------");
-
-                // Clear the data.
-                GameDetails.getGamedetails().clearData();
-            }
-        } else {
-            System.out.println("Cancel button is pressed");
-        }
     }
 }
